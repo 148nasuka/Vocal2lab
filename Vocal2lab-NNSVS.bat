6aad31737d234f7e2eb49bd07c50bb64204967cd
148nasuka <37668571+148nasuka@users.noreply.github.com>
2022-04-30 21:54:35 +0900

    Add batch file

---
+++ Vocal2lab-NNSVS.bat
@@ -0,0 +1,9 @@
+@echo off
+echo ******************************
+echo   Vocal2lab v0.0.2(20220501)
+echo ******************************
+echo NNSVSモードで一括ラベリングを実行します...
+
+python ./Vocal2lab.py --multi nnsvs
+
+pause
